23d8e4df575a7d976e9e6e27f78e28b6851bf960
KoHApaT3 <103876534+KoHApaT3@users.noreply.github.com>
2024-10-30 21:14:09 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SidePanel from './components/Sidepanel/SidePanel';
+
+function App() {
+  return (
+    <div className="App">
+      <SidePanel />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidepanel/SidePanel.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./SidePanel.css";
+
+export default function SidePanel() {
+  return (
+    <aside>aside</aside>
+  )
+}
