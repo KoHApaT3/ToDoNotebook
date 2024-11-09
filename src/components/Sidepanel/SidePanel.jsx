dd627b2305af28d9d1bb7c1cd03c2a5ce9e0d822
KoHApaT3 <103876534+KoHApaT3@users.noreply.github.com>
2024-11-09 20:41:53 +0900

    SidePanelupdate

--- src/components/Sidepanel/SidePanel.jsx
+++ src/components/Sidepanel/SidePanel.jsx
@@ -4,7 +4,11 @@ import "./SidePanel.css";
 export default function SidePanel() {
   return (
     <div className="SidePanel">
-      popa
+      <div className="Content">
+        <button className="NewNote">+ Создать новую запись</button>
+        <input type="text" className='Search' />
+      </div>
+
     </div>
   )
 }
